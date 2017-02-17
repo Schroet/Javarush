33f107297c649f4570d5d444fc879eb254963464
Anatoliy Velikokhatskiy <avel@diceus.com>
2017-02-17 10:33:22 +0200

    First commit

---
+++ Javarush/src/level04/Task10.java
@@ -0,0 +1,18 @@
+package level04;
+
+public class Task10 {
+	
+	   public static void main(String[] args) {
+	        checkInterval(60);
+	        checkInterval(112);
+	    }
+
+	    public static void checkInterval(int a) {
+	        
+	        if (a < 50 || a > 100) {
+	            
+	            System.out.println("Число " + 112 + "не содержится в интервале.");
+	        }
+	        //::CODE:
+	    }
+	}
